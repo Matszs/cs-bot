9a882b27306f2b3b67540be27e05cf3d31851b03
Mats Otten <mats@kepsi.nl>
2016-12-21 21:53:48 +0100

    state on chat level instead of user, tabel 'conversations', 'JaOfNee' actions created, asking and acting on confirmation

--- suusbot.py
+++ suusbot.py
@@ -16,6 +16,7 @@ class STATES:
 	NEW_USER = -1
 	NEW_USER_WELCOME_MESSAGE = -2
 	TUTORIAL = -3
+	NOTIFY_START_QUESTION = 1
 
 db = firebase.FirebaseApplication(FIREBASE_DB, None)
 
@@ -24,24 +25,23 @@ def message_reveiver(msg):
 	if user is False:
 		user = save_user(msg['from'])
 
-	insert = db.post('messages/received/' + str(msg['chat']['id']), msg)
+	conversation = get_conversation(msg['chat']['id'])
+	if conversation is False:
+		conversation = save_conversation(msg['chat']['id'], msg)
 
-	if 'name' in insert:
-		insertID = insert['name']
-	else:
-		insertID = None
+	insert = db.post('conversations/' + str(conversation['chat_id']) + '/messages', msg)
 
 	# get connected company
 	company = company_by_user(msg['from'])
 	if company is False:
-		start_conversation('NO_COMPANY', msg, user)
+		start_conversation('NO_COMPANY', msg, user, conversation)
 		return
 
 	if user['state'] == STATES.TUTORIAL:
-		start_conversation('TUTORIAL', msg, user)
+		start_conversation('TUTORIAL', msg, user, conversation)
 		return
 
-	ai_handler(user, company, msg)
+	ai_handler(user, company, msg, conversation)
 
 def company_by_user(user):
 	companies = db.get('companies', None)
@@ -60,7 +60,6 @@ def get_user(user):
 
 	return user
 def save_user(user):
-	user['state'] = STATES.NEW_USER
 	userDb = db.put('users', str(user['id']), user)
 
 	return userDb
@@ -84,6 +83,27 @@ def get_reservations():
 				reservations[reservationId] = {'reservation': company['reservations'][reservationId], 'company': company}
 
 	return reservations
+
+def save_conversation(chat_id, msg):
+	conversation = {'chat_id': chat_id}
+	conversation['chat'] = msg['chat']
+	conversation['state'] = STATES.DEFAULT
+
+	conversationDb = db.put('conversations', str(chat_id), conversation)
+	return conversationDb
+
+def get_conversation(chat_id, include_messages = False):
+	conversationDb = db.get('conversations', str(chat_id))
+
+	if conversationDb is None:
+		return False
+
+	if include_messages is False:
+		conversationDb['messages'] = []
+
+	return conversationDb
+
+
 def notify_starting_reservations():
 	reservations = get_reservations()
 
@@ -91,7 +111,7 @@ def notify_starting_reservations():
 		reservation = reservations[reservationId]
 
 		if reservation['reservation']['reminder_sent']:
-			break
+			continue
 
 		dateFormatted = datetime.datetime.strptime(reservation['reservation']['date'], "%Y-%m-%d")
 
@@ -101,6 +121,7 @@ def notify_starting_reservations():
 			startTimeMargeEnd = datetime.datetime.strptime(reservation['reservation']['start_time'], '%H:%M:%S')
 
 			startTime = datetime.datetime.strptime(reservation['reservation']['start_time'], '%H:%M:%S')
+
 
 			if (datetime.datetime.now().hour == startTimeMargeStart.hour and datetime.datetime.now().minute >= startTimeMargeStart.minute) or (datetime.datetime.now().hour == startTimeMargeEnd.hour and datetime.datetime.now().minute <= startTimeMargeEnd.minute):
 
@@ -115,13 +136,16 @@ def notify_starting_reservations():
 
 				bot.sendMessage(reservation['reservation']['chat_id'], "Uw afspraak begint zometeen om " + str(startHours) + ":" + str(startMinutes) + ", gaat deze afspraak nog door?")
 				db.put('companies/' + reservation['company']['id'] + '/reservations/' + reservationId, 'reminder_sent', 1)
+
+				conversation = get_conversation(reservation['reservation']['chat_id'])
+				db.put('/conversations/' + str(conversation['chat_id']), 'last_data', {'type': 'confirm_reservation', 'reservation_id': reservationId, 'company': reservation['company']})
+
 				print("reminder sent")
 
-
-def start_conversation(type, msg, user):
+def start_conversation(type, msg, user, conversation):
 
 	if type == 'NO_COMPANY':
-		if user['state'] == STATES.NEW_USER:
+		if conversation['state'] == STATES.NEW_USER:
 			#send the welcome message and ask for the SUUS-ID
 			bot.sendMessage(msg['chat']['id'], "Hallo " + msg['from']['first_name'] + ", mijn naam is Suus!")
 			time.sleep(1)
@@ -130,45 +154,45 @@ def start_conversation(type, msg, user):
 			bot.sendMessage(msg['chat']['id'], "Om te beginnen heb ik je SUUS-ID nodig. Deze krijg je van je leidinggevende.")
 
 			#set user state to know we already sent the welcome message
-			db.put('users/' + str(user['id']), 'state', STATES.NEW_USER_WELCOME_MESSAGE)
-			user['state'] = STATES.NEW_USER_WELCOME_MESSAGE
+			db.put('conversations/' + str(conversation['chat_id']), 'state', STATES.NEW_USER_WELCOME_MESSAGE)
+			conversation['state'] = STATES.NEW_USER_WELCOME_MESSAGE
 
-		if user['state'] == STATES.NEW_USER_WELCOME_MESSAGE:
+		if conversation['state'] == STATES.NEW_USER_WELCOME_MESSAGE:
 			company = get_company_by_code(msg['text'])
 
 			if company is not False:
 				#add user to company
 				db.put('companies/' + company['id'] + '/users', str(user['id']), user['id'])
 
-				#set user state
-				db.put('users/' + str(user['id']), 'state', STATES.DEFAULT)
-				user['state'] = STATES.DEFAULT
+				#set company state
+				db.put('conversations/' + str(conversation['chat_id']), 'state', STATES.DEFAULT)
+				conversation['state'] = STATES.DEFAULT
 
 				#inform user
 				bot.sendMessage(msg['chat']['id'], "Welkom bij " + company['name'] + "!")
 
 				#start tutorial
-				start_conversation('TUTORIAL', msg, user)
+				start_conversation('TUTORIAL', msg, user, conversation)
 			else:
 				print("COMPANY NOT FOUND!!")
 	if type is 'TUTORIAL':
 
-		if user['state'] == STATES.DEFAULT:
-			db.put('users/' + str(user['id']), 'state', STATES.TUTORIAL)
-			user['state'] = STATES.TUTORIAL
+		if conversation['state'] == STATES.DEFAULT:
+			db.put('conversations/' + str(conversation['chat_id']), 'state', STATES.TUTORIAL)
+			conversation['state'] = STATES.TUTORIAL
 
 			bot.sendMessage(msg['chat']['id'], "We kunnen aan de slag! We beginnen met een korte introductie om je te leren hoe het werkt.")
 			time.sleep(2)
 			bot.sendMessage(msg['chat']['id'], "We gaan een vergaderruimte reserveren. Om dit te doen type je: \n\n Ik wil graag een vergaderruimte reserveren \n- of - \n Ik wil graag een vergaderruimte voor morgen", parse_mode="HTML")
 
-		if user['state'] == STATES.TUTORIAL:
+		if conversation['state'] == STATES.TUTORIAL:
 			print("TUTORIAL FASE 2")
 			#todo: get msg[text]
 			#todo: send to AI
 			#todo: check if action is 'reserveren'
 			#todo: If so, put state to default or if you want to have more tutorial steps you have to create different states (STATES.TUTORIAL_1, STATES.TUTORIAL_2, etc...)
 
-def timelyEvents():
+def timely_events():
 
 	while True:
 
@@ -185,6 +209,7 @@ def ai_request(user, text):
 	request.session_id = user['id']
 	request.query = text
 	return json.loads(request.getresponse().read().decode('utf_8'))
+
 def ai_request_handler(request):
 	# todo: get action, params and extract everything, making sure json is always the same
 
@@ -201,14 +226,13 @@ def ai_request_handler(request):
 		returnValue['params'] = request['result']['parameters']
 
 	return returnValue
-def ai_handler(user, company, msg):
+
+def ai_handler(user, company, msg, conversation):
 	request = ai_request(user, msg['text'])
 	formatted = ai_request_handler(request)
 	# todo: switch/if/.. based on formatted data, check actions and perform actions.
 	print(formatted)
-
-	bot.sendMessage(msg['chat']['id'], formatted['message'])
-	print(formatted['action'])
+	postMessage = True
 
 	if formatted['action'] == "Reserveren":
 		start_time = None
@@ -220,12 +244,54 @@ def ai_handler(user, company, msg):
 			end_time = formatted['params']['eind_tijd']
 
 		if start_time is not None and end_time is not None:
-			date = datetime.datetime.now()
-			db.post('companies/' + company['id'] + '/reservations', {'start_time': start_time, 'end_time': end_time, 'user': user['id'], 'date': time.strftime('%Y-%m-%d'), 'created': str(date), 'chat_id': msg['chat']['id'], 'reminder_sent': 0, 'feedback_sent': 0})
+			db.put('/conversations/' + str(conversation['chat_id']), 'last_data', {'type': 'reservation', 'start_time': start_time, 'end_time': end_time})
+			bot.sendMessage(msg['chat']['id'], "Weet u zeker dat u een kamer wilt reserveren voor het volgende: \n\n Starttijd:  " + start_time + " \n Eindtijd:  " + end_time + " \n Kamer:  -")
+			postMessage = False # don't post the AI message, we only want to send the confirmation
+
+	elif formatted['action'] == "JaNeeVraag":
+
+		lastData = db.get('/conversations/' + str(conversation['chat_id']), 'last_data')
+
+		if lastData is not None:
+			if 'type' in lastData:
+
+				if lastData['type'] == 'reservation':
+
+					if formatted['params']['JaOfNee'] == 'Ja':
+						date = datetime.datetime.now()
+						start_time = lastData['start_time']
+						end_time = lastData['end_time']
+
+						db.post('companies/' + company['id'] + '/reservations', {'start_time': start_time, 'end_time': end_time, 'user': user['id'], 'date': time.strftime('%Y-%m-%d'), 'created': str(date), 'chat_id': msg['chat']['id'], 'reminder_sent': 0, 'feedback_sent': 0, 'canceled': 0})
+						bot.sendMessage(msg['chat']['id'], "De reservering staat genoteerd.")
+					else:
+						bot.sendMessage(msg['chat']['id'], "De reservering is geannuleerd.")
+
+				elif lastData['type'] == 'confirm_reservation':
+					if formatted['params']['JaOfNee'] == 'Ja':
+						bot.sendMessage(msg['chat']['id'], "👍🏻")
+					else:
+						db.put('companies/' + lastData['company']['id'] + '/reservations/' + lastData['reservation_id'], 'canceled', 1)
+						bot.sendMessage(msg['chat']['id'], "De reservering is geannuleerd.")
+
+				db.delete('/conversations/' + str(conversation['chat_id']), 'last_data') # delete last_data after processing
+
+			else:
+				print("ERROR: Data format corrupt")
+
+	if formatted['message'] != "" and postMessage:
+		bot.sendMessage(msg['chat']['id'], formatted['message'])
+
+
+
+
+
+
+
 
 def main():
 	# start timely-events
-	t = threading.Thread(target=timelyEvents)
+	t = threading.Thread(target=timely_events)
 	t.daemon = True
 	t.start()
 
